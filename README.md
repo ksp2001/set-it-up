7c10285725804ae63f6007a88d72be5e1c153aad
Ketki Panse <ketki.panse@shopify.com>
2019-11-23 20:18:16 -0500

    Restarting everything due to an error in the previous repo

---
+++ README.md
@@ -0,0 +1,8 @@
+## SET IT UP :: THE APP THAT TELLS YOU EXACTLY WHAT COMPANY IS LOOKING FOR YOU
+
+(This is a Hack Western project)
+
+1. How _Set It Up_ works is simple — you tell it what is your programming language of choice. It'll then scrape indeed to see what companies are currently looking to hire developers using the language you specified, and you give you back the best fit company.
+2. The program determines the best company using machine learning.
+3. The app uses node, brain.js (for the machine learning part), cheerio.js (for scraping indeed) and stores and reads data as a json file.
+4. In progress : adding a frontend
---
+++ scrape.js
@@ -0,0 +1,60 @@
+const request =  require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const brain = require('brain.js');
+const express = require('express');
+const app = express();
+
+let lang = "ruby"
+app.use(express.urlencoded({extended: true}))
+
+app.use(express.static('public'))
+
+app.get('/', function(req, res){
+   res.sendFile('index.html');
+});
+
+app.listen(3000);
+
+app.post('/submit-form', (req, res) => {
+  lang = req.body.language
+
+let companiesList = []
+link_string = "https://ca.indeed.com/jobs?q="+lang+"&l="
+
+request(link_string, (error, response, html)=> {
+  if (!error && response.statusCode == 200) {
+    const $ = cheerio.load(html)
+    $('.company').each((i,el)=> {
+      companiesList[i] = {}
+      const company_name = $(el).text().trim()
+      if (company_name != undefined) {
+      companiesList[i]['language'] = lang;
+      companiesList[i]['company'] = company_name;
+      }
+    })
+  }
+  let json = JSON.stringify(companiesList);
+  fs.writeFile("data.json", json, 'utf8', function (err) {
+    if (err) {
+        console.log("An error occured while writing JSON Object to File.");
+        return console.log(err);
+    }
+
+  let data = require('./data.json');
+  const network = new brain.recurrent.LSTM
+  const training_data = data.map(item => ({
+  input : item.language,
+  output : item.company
+}));
+
+network.train (training_data, {iterations:300});
+
+const output = network.run(lang);
+console.log(`Company: ${output}`);
+res.sendFile('/Users/aestaeric/Desktop/testing-web-scraping/public/result.html');
+fs.unlinkSync('./data.json');
+  });
+
+});
+});
